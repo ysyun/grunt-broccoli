675c0f0f821af75360703c073f53ce2e79f05b58
Kelly Miyashiro <miyashiro.kelly@gmail.com>
2014-04-29 12:25:09 -0700

    Actually support default Brocfile

--- test/broccoli_build_test.js
+++ test/broccoli_build_test.js
@@ -9,6 +9,14 @@ exports.broccoliBuildTests = {
     test.equal(main2, "main2");
     test.done();
   },
+  withDefault: function(test) {
+    test.expect(2);
+    var main1 = grunt.file.read('./tmp/tests/with-default/main1');
+    var main2 = grunt.file.read('./tmp/tests/with-default/main2');
+    test.equal(main1, "main1");
+    test.equal(main2, "main2");
+    test.done();
+  },
   withFunction: function(test) {
     test.expect(2);
     var main1 = grunt.file.read('./tmp/tests/with-function/main1');
