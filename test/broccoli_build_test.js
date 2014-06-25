442d88636e23ad99cb15779c189029baa11f5061
Martin Muñoz <im.mmun@gmail.com>
2014-06-24 20:02:53 -0400

    Fix tests
    We now shell out to grunt inside of each test instead of running grunt
    once and then executing all the tests.

--- test/broccoli_build_test.js
+++ test/broccoli_build_test.js
@@ -1,28 +1,41 @@
 var grunt = require('grunt');
+var spawn = require('child_process').spawn;
+
+function runGruntTaskForTest(testName, mode, callback) {
+  var taskName = 'broccoli:' + testName + ':' + mode;
+  var child = spawn('grunt', [taskName]);
+  child.on('close', callback);
+}
 
 exports.broccoliBuildTests = {
   withBrocfile: function(test) {
     test.expect(2);
-    var main1 = grunt.file.read('./tmp/tests/with-brocfile/main1');
-    var main2 = grunt.file.read('./tmp/tests/with-brocfile/main2');
-    test.equal(main1, "main1");
-    test.equal(main2, "main2");
-    test.done();
+    runGruntTaskForTest('with-brocfile', 'build', function() {
+      var main1 = grunt.file.read('./tmp/tests/with-brocfile/main1');
+      var main2 = grunt.file.read('./tmp/tests/with-brocfile/main2');
+      test.equal(main1, "main1");
+      test.equal(main2, "main2");
+      test.done();
+    });
   },
   withDefault: function(test) {
     test.expect(2);
-    var main1 = grunt.file.read('./tmp/tests/with-default/main1');
-    var main2 = grunt.file.read('./tmp/tests/with-default/main2');
-    test.equal(main1, "main1");
-    test.equal(main2, "main2");
-    test.done();
+    runGruntTaskForTest('with-default', 'build', function() {
+      var main1 = grunt.file.read('./tmp/tests/with-default/main1');
+      var main2 = grunt.file.read('./tmp/tests/with-default/main2');
+      test.equal(main1, "main1");
+      test.equal(main2, "main2");
+      test.done();
+    });
   },
   withFunction: function(test) {
     test.expect(2);
-    var main1 = grunt.file.read('./tmp/tests/with-function/main1');
-    var main2 = grunt.file.read('./tmp/tests/with-function/main2');
-    test.equal(main1, "main1");
-    test.equal(main2, "main2");
-    test.done();
+    runGruntTaskForTest('with-function', 'build', function() {
+      var main1 = grunt.file.read('./tmp/tests/with-function/main1');
+      var main2 = grunt.file.read('./tmp/tests/with-function/main2');
+      test.equal(main1, "main1");
+      test.equal(main2, "main2");
+      test.done();
+    });
   }
 };
